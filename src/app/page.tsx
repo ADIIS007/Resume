f1a25fb6ef81cb23f372d879c4cc7e6b40daadb6
ADIIS007 <20B81A05D1@gmail.com>
2023-12-16 23:21:35 +0530

    Try 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
-import Link from "next/link";
-import React from "react";
-import Particles from "../components/public/particles";
+import Link from 'next/link';
+import React from 'react';
+import Particles from '../components/public/particles';
 
 // This will make up the home page
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+    <div className='flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black'>
+      <div className='hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
       <Particles
-        className="absolute inset-0 -z-10 animate-fade-in"
+        className='absolute inset-0 -z-10 animate-fade-in'
         quantity={100}
       />
-      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+      <h1 className='z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text '>
         ADIIS007
       </h1>
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-          I'm building this{" "}
+      <div className='hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
+      <div className='my-16 text-center animate-fade-in'>
+        <h2 className='text-sm text-zinc-500 '>
+          I'm building this{' '}
           {/* <Link
             target="_blank"
             href="https://unkey.dev"
@@ -26,9 +26,9 @@ export default function Home() {
           >
             unkey.dev 
           </Link>  */}
-           to build their own site for developers.
+          to build their own site for developers.
         </h2>
       </div>
     </div>
-  )
+  );
 }
