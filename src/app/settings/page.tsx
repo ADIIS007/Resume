51726ce2ad2cd7b973e1067f025bbb0618cd9760
ADIIS007 <20B81A05D1@gmail.com>
2023-12-17 11:29:19 +0530

    UI update required

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -7,8 +7,8 @@ function SettingsPage() {
   return (
     <div className='p-8'>
       <Particles
-        className='absolute inset-0 -z-10 animate-fade-in'
-        quantity={100}
+        className='absolute inset-0 -z-10 animate-fade-in min-h-full'
+        quantity={1000}
       />
       <div className='relative pb-16 '>
         <div className='grid grid-cols-1 gap-8 mx-auto'>
@@ -149,8 +149,8 @@ function SettingsPage() {
                   <div className='grid grid-cols-4 gap-2'>
                     <div className='outline-none border border-gray-400 rounded px-4 py-2 mx-2 w-55 h-55 place-items-center'>
                       <AnimatedLogo
-                        imgName={'nodejs'}
-                        imgSrc={'techstack/Angular.svg'}
+                        imgName={'Angular'}
+                        imgSrc={'techstack/angular.svg'}
                       />
                     </div>
                     <div className='outline-none border border-gray-400 rounded px-4 py-2 mx-2 w-55 h-55 place-items-center'>
@@ -177,6 +177,84 @@ function SettingsPage() {
             </article>
           </Card>
         </div>
+      </div>
+
+      <div className='grid grid-cols-1 gap-8 mx-auto pb-16'>
+        <Card>
+          <article className='relative w-full h-full p-4 md:p-8'>
+            <div className='justify-center text-center'>
+              <h1 className='text-1xl text-zinc-100 group-hover:text-white sm:text-4xl font-display p-4'>
+                Acadamic Life
+              </h1>
+              <br />
+              <div className='grid grid-cols-1 gap-4'>
+                <div className='grid grid-rows-1 grid-flow-col gap-4 outline-none border border-gray-400 rounded p-3'>
+                  <div>Year</div>
+                  <div>Syllabus</div>
+                  <div>Specialization</div>
+                  <div>Grade</div>
+                </div>
+                <div className='grid grid-rows-1 grid-flow-col gap-4 outline-none border border-gray-400 rounded p-3'>
+                  <div>2024</div>
+                  <div>JNTU-H</div>
+                  <div>Computer science</div>
+                  <div>Btech</div>
+                </div>
+                <div className='grid grid-rows-1 grid-flow-col gap-4 outline-none border border-gray-400 rounded p-3'>
+                  <div>2020</div>
+                  <div>CBSE</div>
+                  <div>MPC-CS</div>
+                  <div>XII</div>
+                </div>
+                <div className='grid grid-rows-1 grid-flow-col gap-4 outline-none border border-gray-400 rounded p-3'>
+                  <div>2018</div>
+                  <div>CBSE</div>
+                  <div>N/A</div>
+                  <div>X</div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </Card>
+      </div>
+
+      <div className='grid grid-cols-1 gap-8 mx-auto pb-16'>
+        <Card>
+          <article className='relative w-full h-full p-4 md:p-8'>
+            <div className='justify-center text-center'>
+              <h1 className='text-1xl text-zinc-100 group-hover:text-white sm:text-4xl font-display p-4'>
+                Acadamic Life
+              </h1>
+              <br />
+              <div className='grid grid-cols-1 gap-4'>
+                <div className='grid grid-rows-1 grid-flow-col gap-4 outline-none border border-gray-400 rounded p-3'>
+                  <div>Year</div>
+                  <div>Syllabus</div>
+                  <div>Specialization</div>
+                  <div>Grade</div>
+                </div>
+                <div className='grid grid-rows-1 grid-flow-col gap-4 outline-none border border-gray-400 rounded p-3'>
+                  <div>2024</div>
+                  <div>JNTU-H</div>
+                  <div>Computer science</div>
+                  <div>Btech</div>
+                </div>
+                <div className='grid grid-rows-1 grid-flow-col gap-4 outline-none border border-gray-400 rounded p-3'>
+                  <div>2020</div>
+                  <div>CBSE</div>
+                  <div>MPC-CS</div>
+                  <div>XII</div>
+                </div>
+                <div className='grid grid-rows-1 grid-flow-col gap-4 outline-none border border-gray-400 rounded p-3'>
+                  <div>2018</div>
+                  <div>CBSE</div>
+                  <div>N/A</div>
+                  <div>X</div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </Card>
       </div>
     </div>
   );
--- src/components/public/particles.tsx
+++ src/components/public/particles.tsx
@@ -83,8 +83,8 @@ export default function Particles({
 	const resizeCanvas = () => {
 		if (canvasContainerRef.current && canvasRef.current && context.current) {
 			circles.current.length = 0;
-			canvasSize.current.w = canvasContainerRef.current.offsetWidth;
-			canvasSize.current.h = canvasContainerRef.current.offsetHeight;
+			canvasSize.current.w = window.innerWidth;
+			canvasSize.current.h = window.innerHeight;
 			canvasRef.current.width = canvasSize.current.w * dpr;
 			canvasRef.current.height = canvasSize.current.h * dpr;
 			canvasRef.current.style.width = `${canvasSize.current.w}px`;
